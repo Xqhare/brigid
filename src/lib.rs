037afadca7d6368af8550bf44eadb8726872b15c
Xqhare <xqhare@web.de>
2026-04-04 01:11:00 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+#![doc = include_str!("../README.md")]
+#![warn(missing_docs)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::all)]
+#![warn(clippy::restriction)]
+#![allow(
+    clippy::missing_docs_in_private_items,
+    clippy::print_stdout,
+    clippy::implicit_return
+)]
+
+/// Adds two numbers
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
