6397bab4093bdb5fd8bcf4c7737cf57dacd9196b
Xqhare <xqhare@web.de>
2026-04-06 02:46:04 +0200

    make clippy happy

--- src/content.rs
+++ src/content.rs
@@ -8,11 +8,11 @@ use crate::error::BrigidResult;
 /// Represents the different content types that Brigid can handle
 #[derive(Debug, Clone)]
 pub enum Content {
-    /// CSV content stored as an XffValue
+    /// CSV content stored as a `XffValue`
     CSV(XffValue),
-    /// XFF content stored as an XffValue
+    /// XFF content stored as a `XffValue`
     XFF(XffValue),
-    /// JSON content stored as an XffValue
+    /// JSON content stored as a `XffValue`
     JSON(XffValue),
 }
 
@@ -23,6 +23,7 @@ impl Content {
     ///
     /// The `XffValue` representation of the content.
     #[must_use]
+    #[inline]
     pub fn into_xff(self) -> XffValue {
         match self {
             Content::CSV(xff) | Content::XFF(xff) | Content::JSON(xff) => xff,
@@ -41,12 +42,14 @@ impl Content {
     /// # Errors
     ///
     /// Returns a `BrigidError::Mawu` or `BrigidError::Nabu` if the file cannot be saved.
+    #[allow(clippy::absolute_paths)]
+    #[inline]
     pub fn save(self, path: &Path) -> BrigidResult<()> {
         match self {
             Content::CSV(xff) => {
                 let mawu_val = if let XffValue::Array(rows) = xff {
                     let mut csv_rows = Vec::new();
-                    for row in rows.into_iter() {
+                    for row in rows {
                         if let XffValue::Array(cols) = row {
                             csv_rows.push(cols.into_vec());
                         } else {
@@ -59,14 +62,14 @@ impl Content {
                 };
                 let contents = MawuContents::Csv(mawu_val);
                 if let Err(err) = mawu::write(path, contents) {
-                    return Err(err.into());
+                    Err(err.into())
                 } else {
                     Ok(())
                 }
             }
             Content::XFF(xff) => {
                 if let Err(err) = nabu::serde::write(path, xff) {
-                    return Err(err.into());
+                    Err(err.into())
                 } else {
                     Ok(())
                 }
@@ -74,7 +77,7 @@ impl Content {
             Content::JSON(xff) => {
                 let contents = MawuContents::Json(xff);
                 if let Err(err) = mawu::write_pretty(path, contents, 2) {
-                    return Err(err.into());
+                    Err(err.into())
                 } else {
                     Ok(())
                 }
